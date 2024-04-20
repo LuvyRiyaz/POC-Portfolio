1e5e004f0e724abd51dc884ed55601ca181d806c
LuvyRiyaz <riyaj4606@gmail.com>
2024-04-20 11:39:24 +0530

    addig about and contact contant

---
+++ portfolio/src/components/contact/Contact.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Add your form submission logic here, such as sending the form data to a backend API
+    console.log(formData);
+    // Clear form fields after submission
+    setFormData({
+      name: "",
+      email: "",
+      message: ""
+    });
+  };
+
+  return (
+      <div className="py-4">
+        <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border text-black	rounded-md focus:outline-none focus:border-purple-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 text-black"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block mb-2">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border text-black	rounded-md resize-none focus:outline-none focus:border-purple-500"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">Submit</button>
+        </form>
+      </div>
+  );
+}
---
+++ portfolio/src/pages/contact.js
@@ -0,0 +1,15 @@
+import Layout from '../components/layout/Layout.jsx';
+import Contact from '@/components/contact/Contact.js';
+
+export default function contact() {
+  return (
+    <Layout>
+     <div>
+      <Contact />
+    </div>
+    </Layout>
+  );
+}
+
+
+
